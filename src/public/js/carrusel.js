c019443e80af4405ed175bd78ee140cfd1487c10
pjc1288 <pjc.1288@gmail.com>
2021-02-02 21:01:54 +0100

    añadido carrusel userDash revisar

---
+++ src/public/js/carrusel.js
@@ -0,0 +1,10 @@
+///funcion carrusel
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
